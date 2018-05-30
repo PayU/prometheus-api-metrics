51721257c56c42e192d4f8e772d5c77247447023
idantovi <Idan.Tovi@zooz.com>
2018-05-30 13:04:22 +0300

    fix route extraction

--- src/metrics-middleware.js
+++ src/metrics-middleware.js
@@ -4,7 +4,7 @@ const Prometheus = require('prom-client');
 require('pkginfo')(module, 'version', 'name');
 const debug = require('debug')(module.exports.name);
 const appVersion = module.exports.version;
-let metricsInterval, path;
+let metricsInterval, path, responseTimeHistogram, requestSizeHistogram, responseSizeHistogram, onExitEvent;
 
 module.exports = (options = {}) => {
     const { metricsPath, defaultMetricsInterval, durationBuckets, requestSizeBuckets, responseSizeBuckets } = options;
@@ -22,7 +22,7 @@ module.exports = (options = {}) => {
     const versionSegments = appVersion.split('.').map(Number);
     version.labels(appVersion, versionSegments[0], versionSegments[1], versionSegments[2]).set(1);
 
-    new Prometheus.Histogram({
+    responseTimeHistogram = new Prometheus.Histogram({
         name: 'http_request_duration_ms',
         help: 'Duration of HTTP requests in ms',
         labelNames: ['method', 'route', 'code'],
@@ -30,21 +30,23 @@ module.exports = (options = {}) => {
         buckets: durationBuckets || [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
     });
 
-    new Prometheus.Histogram({
+    requestSizeHistogram = new Prometheus.Histogram({
         name: 'http_request_size_bytes',
         help: 'Duration of HTTP requests in ms',
         labelNames: ['method', 'route', 'code'],
         buckets: requestSizeBuckets || [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000]
     });
 
-    new Prometheus.Histogram({
+    responseSizeHistogram = new Prometheus.Histogram({
         name: 'http_response_size_bytes',
         help: 'Duration of HTTP requests in ms',
         labelNames: ['method', 'route', 'code'],
         buckets: responseSizeBuckets || [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000]
     });
 
-    process.on('exit', _clearDefaultMetricsInternal);
+    if (!onExitEvent) {
+        onExitEvent = process.on('exit', _clearDefaultMetricsInternal);
+    }
 
     return middleware;
 };
@@ -78,11 +80,31 @@ function _handleResponse (req, res) {
     const responseTime = Date.now() - req.metrics.startEpoch;
     const responseLength = parseInt(res.get('Content-Length')) || 0;
 
-    Prometheus.register.getSingleMetric('http_request_size_bytes').observe({ method: req.method, route: req.route.path, code: res.statusCode }, req.metrics.contentLength);
-    Prometheus.register.getSingleMetric('http_request_duration_ms').observe({ method: req.method, route: req.route.path, code: res.statusCode }, responseTime);
-    Prometheus.register.getSingleMetric('http_response_size_bytes').observe({ method: req.method, route: req.route.path, code: res.statusCode }, responseLength);
+    const route = _getRoute(req);
+
+    requestSizeHistogram.observe({ method: req.method, route: route, code: res.statusCode }, req.metrics.contentLength);
+    responseTimeHistogram.observe({ method: req.method, route: route, code: res.statusCode }, responseTime);
+    responseSizeHistogram.observe({ method: req.method, route: route, code: res.statusCode }, responseLength);
 
     debug(`metrics updated, request length: ${req.metrics.contentLength}, response length: ${responseLength}, response time: ${responseTime}`);
+}
+
+function _getRoute(req) {
+    var route = req.baseUrl; // express
+    if (req.swagger) { // swagger
+        route = req.swagger.apiPath;
+    } else if (req.route && route) { // express
+        if (req.route.path !== '/') {
+            route = route + req.route.path;
+        }
+    } else if (req.url && !route) { // restify
+        route = req.url;
+        if (req.route) {
+            route = req.route.path;
+        }
+    }
+
+    return route;
 }
 
 function _clearDefaultMetricsInternal() {
