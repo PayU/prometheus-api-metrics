1d60ebe8cc009179afa30015033c7f9243522f0e
Royvaknin29 <Royvaknin29@gmail.com>
2018-10-14 14:38:05 +0300

    Use only values grater than 1 for request duration in ms

--- src/metrics-middleware.js
+++ src/metrics-middleware.js
@@ -31,8 +31,8 @@ module.exports = (options = {}) => {
             name: 'http_request_duration_ms',
             help: 'Duration of HTTP requests in ms',
             labelNames: ['method', 'route', 'code'],
-            // buckets for response time from 0.1ms to 500ms
-            buckets: durationBuckets || [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+            // buckets for response time from 1ms to 500ms
+            buckets: durationBuckets || [1, 5, 15, 50, 100, 200, 300, 400, 500]
         });
     }
 
