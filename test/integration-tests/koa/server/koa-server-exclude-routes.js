c82322b33df4abb6a4dbbf59d26d319fa8cab409
Kobi Carmeli <kobi.carmeli@gmail.com>
2020-09-02 12:18:37 +0300

    update deps (#54)
    * updated deps
    
    * replaced sleep package with promisified setTimeout

--- test/integration-tests/koa/server/koa-server-exclude-routes.js
+++ test/integration-tests/koa/server/koa-server-exclude-routes.js
@@ -6,7 +6,7 @@ const app = new Koa();
 const middleware = require('../../../../src/index.js').koaMiddleware;
 const Router = require('koa-router');
 const router = new Router();
-const sleep = require('sleep');
+const sleep = require('../../../utils/sleep');
 
 app.use(async (ctx, next) => {
     try {
@@ -23,29 +23,29 @@ app.use(bodyParser());
 
 app.use(router.routes());
 
-router.get('', (ctx, next) => {
-    sleep.msleep(Math.round(Math.random() * 200));
+router.get('', async (ctx, next) => {
+    await sleep(Math.round(Math.random() * 200));
     ctx.body = { message: 'Hello World!' };
     ctx.status = 200;
     next();
 });
 
-router.get('/hello', (ctx, next) => {
-    sleep.msleep(Math.round(Math.random() * 200));
+router.get('/hello', async (ctx, next) => {
+    await sleep(Math.round(Math.random() * 200));
     ctx.body = { message: 'Hello World!' };
     ctx.status = 200;
     next();
 });
 
-router.get('/health', (ctx, next) => {
-    sleep.msleep(ctx.request.body.delay || 1);
+router.get('/health', async (ctx, next) => {
+    await sleep(ctx.request.body.delay || 1);
     ctx.body = { message: 'Hello World!' };
     ctx.status = 200;
     next();
 });
 
-router.get('/health/:id', (ctx, next) => {
-    sleep.msleep(ctx.request.body.delay || 1);
+router.get('/health/:id', async (ctx, next) => {
+    await sleep(ctx.request.body.delay || 1);
     ctx.body = { message: 'Hello World!' };
     ctx.status = 200;
     next();
