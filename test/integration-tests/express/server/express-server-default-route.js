86f18b3dd789430e993b2ac8599896b341ae8e6e
Kenn Sippell <kennsippell@gmail.com>
2023-06-28 01:35:18 -0700

    Simpler test server

--- test/integration-tests/express/server/express-server-default-route.js
+++ test/integration-tests/express/server/express-server-default-route.js
@@ -6,16 +6,9 @@ const middleware = require('../../../../src/index.js').expressMiddleware;
 const app = express();
 app.use(middleware({ includeQueryParams: true }));
 
-app.all('*', (req, res, next) => {
+app.all('*', (req, res) => {
     res.status(200);
     res.json({ message: 'Hello World!' });
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-    res.statusCode = 500;
-    // Do not expose your error in production
-    res.json({ error: err.message });
 });
 
 module.exports = app;
